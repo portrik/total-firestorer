8aa7a40cff103525d36d46a4e530405d342add65
Patrik Dvořáček <patrik@dvoracek.dev>
2024-08-07 19:26:33 +0200

    fix: 💚 Fix failing pipeline. (#2)
    :shipit:

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,17 @@
 import { defineConfig } from '@vscode/test-cli';
 
+const isCI = process.env['CI'] !== undefined;
+
 export default defineConfig({
 	files: 'out/test/**/*.test.js',
+	coverage: {
+		reporter: ['mocha-junit-reporter', 'spec'],
+		output: './coverage',
+	},
+	mocha: {
+		reporter: isCI ? 'mocha-junit-reporter' : 'spec',
+		reporterOptions: {
+			mochaFile: './junit.xml',
+		}
+	}
 });
--- README.md
+++ README.md
@@ -16,8 +16,4 @@ TODO:
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release!
+TODO:
